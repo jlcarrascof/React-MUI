4d25c6d537391676edcbc3012406fde82b803cc1
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-22 19:40:59 -0400

    Add grid layout to GridDemo component for improved structure

--- recipe-sharing-app/src/practices/GridDemo.jsx
+++ recipe-sharing-app/src/practices/GridDemo.jsx
@@ -5,7 +5,16 @@ import Box from '@mui/material/Box'
 function GridDemo() {
   return (
     <div>
-
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Box sx={{ backgroundColor: 'lightcoral', padding: '20px' }} >
+            Column 1
+          </Box>
+        </Grid>
+        <Grid item xs={6}>
+          <Box sx={{ backgroundColor: 'lightblue', height: '100px' }} />
+        </Grid>
+      </Grid>
     </div>
   )
 }
