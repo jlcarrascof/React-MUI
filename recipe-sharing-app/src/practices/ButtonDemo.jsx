53d94c25d8e6929815c0090a160b4028a9b93dbc
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-22 18:38:57 -0400

    Add success button variant to ButtonDemo component

--- recipe-sharing-app/src/practices/ButtonDemo.jsx
+++ recipe-sharing-app/src/practices/ButtonDemo.jsx
@@ -6,7 +6,7 @@ function ButtonDemo() {
     <div>
         <Button variant='contained' color='primary'>Primary</Button>
         <Button variant='outlined' color='secondary'>Secondary</Button>
-
+        <Button variant='text' color='success'>Success</Button>
     </div>
   )
 }
