01ec77142025d07e84808c61086915361f8cf427
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-22 18:37:49 -0400

    Add primary button to ButtonDemo component

--- recipe-sharing-app/src/practices/ButtonDemo.jsx
+++ recipe-sharing-app/src/practices/ButtonDemo.jsx
@@ -1,8 +1,10 @@
+import { Button } from '@mui/material'
 import React from 'react'
 
 function ButtonDemo() {
   return (
     <div>
+        <Button variant='contained' color='primary'>Primary</Button>
     </div>
   )
 }
