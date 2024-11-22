3d070b4055aa82cef87049bc33398735482d4b8b
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-22 18:39:21 -0400

    Add ButtonDemo component to Practices for MUI component testing

--- recipe-sharing-app/src/practices/Practices.jsx
+++ recipe-sharing-app/src/practices/Practices.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import ButtonDemo from './ButtonDemo'
 
 function Practices() {
   return (
     <div>
       <h1>Testing MUI components</h1>
+      <ButtonDemo />
     </div>
   )
 }
