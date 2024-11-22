41f2fc96aeb573b0de83b552819788ef0d3d64ac
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-22 19:28:10 -0400

    Add styled Box component to BoxDemo for enhanced presentation

--- recipe-sharing-app/src/practices/BoxDemo.jsx
+++ recipe-sharing-app/src/practices/BoxDemo.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Box } from '@mui/material'
 
 function BoxDemo() {
   return (
     <div>
-
+        <Box sx={{ padding: '20px', backgroundColor: 'lightblue', textAlign: 'center' }}>
+            This is a styled box
+        </Box>
     </div>
   )
 }
