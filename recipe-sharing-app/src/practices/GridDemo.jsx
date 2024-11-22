2aef6f72770ad9cf66f3a6fcf47a7382e6518637
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-22 19:32:37 -0400

    Add GridDemo component as a placeholder for grid demonstrations

---
+++ recipe-sharing-app/src/practices/GridDemo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Grid } from '@mui/material'
+import Box from '@mui/material/Box'
+
+function GridDemo() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default GridDemo
