54c2d9de76c8aeefeef20e3c443fa8397bab7780
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-22 18:22:04 -0400

    Add Counter component to App for displaying a simple counter

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Greeting from './components/Greeting'
+import Counter from './components/Counter'
 
 function App() {
 
   return (
     <div>
       <Greeting name="Student" />
+      <Counter />
     </div>
   )
 }
