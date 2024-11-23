2cf91323108fda6d90bef73a710520ba66053cbe
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-22 21:41:08 -0400

    Enhance RecipeCard component to include image display and CardContent for improved layout

--- recipe-sharing-app/src/components/RecipeCard.jsx
+++ recipe-sharing-app/src/components/RecipeCard.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import { Card } from '@mui/material'
+import { Card, CardContent } from '@mui/material'
 
 function RecipeCard({ title, image }) {
   return (
     <Card>
+        <CardMedia
+            component="img"
+            height="140"
+            image={image}
+            alt={title}
+            sx={{ objectFit: 'cover' }}
+        />
+        <CardContent>
 
+        </CardContent>
     </Card>
 
   )
