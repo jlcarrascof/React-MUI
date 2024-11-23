2407aa9588d3e905617c07db4bb954198a35fc4e
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-22 20:44:17 -0400

    Add Toolbar component to Header for improved layout

--- recipe-sharing-app/src/components/Header.jsx
+++ recipe-sharing-app/src/components/Header.jsx
@@ -1,11 +1,13 @@
-import { AppBar } from '@mui/material'
+import { AppBar, Toolbar } from '@mui/material'
 import React from 'react'
 
 function Header() {
   return (
     <div>
         <AppBar position="static">
+            <Toolbar>
 
+            </Toolbar>
         </AppBar>
     </div>
   )
