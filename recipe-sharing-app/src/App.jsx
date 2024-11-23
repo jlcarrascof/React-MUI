bafa2fa63dba99daec823a4a2f6dc5699b75bc0d
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-22 20:42:57 -0400

    Replace Practices component with Header in App for improved layout

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import Practices from './practices/Practices'
-// import Greeting from './components/Greeting'
-// import Counter from './components/Counter'
+import Header from './components/Header'
+
 
 function App() {
 
   return (
-    <Practices />
+    <Header />
   )
 }
 
