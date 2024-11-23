35467a38d556d8f9aa4232775309c80f1e48083c
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-22 21:01:55 -0400

    Add HomePage component and integrate it into App for future content display

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './components/Header'
+import HomePage from './pages/HomePage'
 
 
 function App() {
 
   return (
     <Header />
+    <HomePage />
   )
 }
 
--- recipe-sharing-app/src/pages/HomePage.jsx
+++ recipe-sharing-app/src/pages/HomePage.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
 function HomePage() {
-  return (
-    <div>
 
-    </div>
-  )
+    const recipes = [
+        { id: 1, title: 'Caesar Salad', image: '/images/cesar.jpg' },
+        { id: 2, title: 'Tomato Soup', image: '/images/tomato.jpg' },
+        { id: 3, title: 'Margarita Pizza', image: '/images/pizza.jpg' },
+    ];
+
+    return (
+        <div>
+
+        </div>
+    )
 }
 
 export default HomePage
