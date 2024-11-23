8a8af07172483dc28ebe73dbcdbc862d94adf281
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-22 21:48:17 -0400

    Update recipe images in HomePage component to use PNG format

--- recipe-sharing-app/src/pages/HomePage.jsx
+++ recipe-sharing-app/src/pages/HomePage.jsx
@@ -5,9 +5,9 @@ import RecipeCard from '../components/RecipeCard'
 function HomePage() {
 
     const recipes = [
-        { id: 1, title: 'Caesar Salad', image: '/images/cesar.jpg' },
-        { id: 2, title: 'Tomato Soup', image: '/images/tomato.jpg' },
-        { id: 3, title: 'Margarita Pizza', image: '/images/pizza.jpg' },
+        { id: 1, title: 'Caesar Salad', image: '/images/cesar.png' },
+        { id: 2, title: 'Tomato Soup', image: '/images/tomato.png' },
+        { id: 3, title: 'Margarita Pizza', image: '/images/pizza.png' },
     ]
 
     return (
