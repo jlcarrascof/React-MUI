bd012a5dca04a4d54f94e1221920bf8830e9f10f
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-22 20:38:19 -0400

    Add AppBar component to Header for improved layout

--- recipe-sharing-app/src/components/Header.jsx
+++ recipe-sharing-app/src/components/Header.jsx
@@ -1,9 +1,12 @@
+import { AppBar } from '@mui/material'
 import React from 'react'
 
 function Header() {
   return (
     <div>
+        <AppBar position="static">
 
+        </AppBar>
     </div>
   )
 }
