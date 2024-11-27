ec5eee15e0b3a44fba0abcb976e8a162d12451d8
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-27 18:33:02 -0400

    Refactor Header component to implement routing links for Home and Add Recipe

--- recipe-sharing-app/src/components/Header.jsx
+++ recipe-sharing-app/src/components/Header.jsx
@@ -4,18 +4,19 @@ import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <div>
-        <AppBar position="static">
-            <Toolbar>
-                <Typography variant="h6" sx={{ flexGrow: 1 }}>
-                    Recipe Sharing App
-                </Typography>
-                <Button color="inherit">Home</Button>
-                <Button color="inherit">Recipes</Button>
-                <Button color="inherit">Contact</Button>
-            </Toolbar>
-        </AppBar>
-    </div>
+    <AppBar position='static'>
+      <Toolbar>
+        <Typography variant='h6' sx={{ flexGrow: 1 }}>
+          Recipe Sharing App
+        </Typography>
+        <Link to='/' style={{ textDecoration: 'none', color: 'white', marginRight: '20px' }}>
+          Home
+        </Link>
+        <Link to='/add-recipe' style={{ textDecoration: 'none', color: 'white', marginRight: '20px' }}>
+          Add Recipe
+        </Link>
+      </Toolbar>
+    </AppBar>
   )
 }
 
