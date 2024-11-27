6932cdb94847dc39db7a138bdfebffd2494ca265
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-27 19:33:14 -0400

    Refactor theme configuration to include component style overrides for MuiAppBar

--- recipe-sharing-app/src/theme.js
+++ recipe-sharing-app/src/theme.js
@@ -1,20 +1,29 @@
-import { createTheme } from '@mui/material/styles'
+import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
-    palette: {
-      mode: 'light', // Let's beging with the light mode by default.
-      primary: {
-        main: '#4caf50', // Green
-      },
-      secondary: {
-        main: '#ff5722', // Orange
+  palette: {
+    mode: 'light', // Modo claro por defecto
+    primary: {
+      main: '#4caf50', // Verde
+    },
+    secondary: {
+      main: '#ff5722', // Naranja
+    },
+  },
+  components: {
+    MuiAppBar: {
+      styleOverrides: {
+        colorPrimary: {
+          backgroundColor: '#ff5722', // Naranja
+        },
       },
     },
-    typography: {
-        fontFamily: 'Roboto, Arial, sans-serif',
-        h1: { fontSize: '2.5rem', fontWeight: 700 },
-        h6: { fontSize: '1.25rem', fontWeight: 500 },
-    },
-})
+  },
+  typography: {
+    fontFamily: 'Roboto, Arial, sans-serif',
+    h1: { fontSize: '2.5rem', fontWeight: 700 },
+    h6: { fontSize: '1.25rem', fontWeight: 500 },
+  },
+});
 
-export default theme
+export default theme;
