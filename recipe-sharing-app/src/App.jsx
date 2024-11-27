e3b9d713c806cb08da4dad2eb394d902681bf4a7
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-27 17:02:39 -0400

    Integrate MUI theme provider and CssBaseline into App component

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { ThemeProvider } from '@mui/material/styles'
+import { CssBaseline } from '@mui/material/CssBaseline'
+import theme from './theme'
 import Header from './components/Header'
 import HomePage from './pages/HomePage'
 
