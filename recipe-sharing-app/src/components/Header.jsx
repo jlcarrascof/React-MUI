e33c0e319b7062e2b8b8a0f73b93fa44d83edeef
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-27 19:20:15 -0400

    Add dark mode toggle state to Header component

--- recipe-sharing-app/src/components/Header.jsx
+++ recipe-sharing-app/src/components/Header.jsx
@@ -1,8 +1,10 @@
-import { AppBar, Toolbar, Typography } from '@mui/material'
-import React from 'react'
+import { AppBar, Toolbar, Typography, Button } from '@mui/material'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Header() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
     <AppBar position='static'>
       <Toolbar>
