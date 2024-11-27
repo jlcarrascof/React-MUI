eaedf0e5bd2be39dd6d1f9374606bde5c06f158e
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-27 17:27:22 -0400

    Import RecipeDetailsPage component in App for routing

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import HomePage from './pages/HomePage'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import AddRecipePage from './pages/AddRecipePage'
+import RecipeDetailsPage from './components/RecipeDetailsPage'
 
 function App() {
 
