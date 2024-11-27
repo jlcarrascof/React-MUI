e4131d620326b6232903e471c8d22ca3d63032eb
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-27 18:39:20 -0400

    Enhance RecipeDetailsPage to display recipe ID and additional information

--- recipe-sharing-app/src/components/RecipeDetailsPage.jsx
+++ recipe-sharing-app/src/components/RecipeDetailsPage.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 function RecipeDetailsPage() {
+  const { id } = useParams()
+
   return (
-    <div>RecipeDetailsPage</div>
+    <div>
+      <h1>Recipe Details</h1>
+      <p>You're in the recipe with ID: { id }</p>
+      <p>Here you will see more information about the recipe in the next videos</p>
+    </div>
   )
 }
 
