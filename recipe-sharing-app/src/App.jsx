b3f1bf2a7deaa11bbdc60b4594f3aa0c1a0c2c83
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-27 17:20:26 -0400

    Remove ThemeProvider and CssBaseline from App component

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,19 +1,16 @@
 import React from 'react'
-import { ThemeProvider } from '@mui/material/styles'
-import { CssBaseline } from '@mui/material/CssBaseline'
-import theme from './theme'
 import Header from './components/Header'
 import HomePage from './pages/HomePage'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 
 function App() {
 
   return (
-    <ThemeProvider>
-      <CssBaseline />
+    <div>
       <Header />
       <HomePage />
-    </ThemeProvider>
+    </div>
   )
 }
 
