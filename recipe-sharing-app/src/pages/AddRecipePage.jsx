7ddd33a91b2372fadc29a00dee0304c85b6c01a2
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-29 13:19:15 -0400

    Add TextField for recipe title input in AddRecipePage component

--- recipe-sharing-app/src/pages/AddRecipePage.jsx
+++ recipe-sharing-app/src/pages/AddRecipePage.jsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material'
+import { Box, TextField } from '@mui/material'
 import React from 'react'
 import { useForm } from 'react-hook-form'
 
@@ -12,7 +12,15 @@ function AddRecipePage() {
   return (
     <Box sx={{ maxWidth: 400, mx: 'auto', mt: 4 }} >
       <form onSubmit={handleSubmit(onSubmit)}>
-        
+        <TextField
+          label="Recipe Title"
+          variant="outlined"
+          fullWidth
+          {...register('title', { required: 'Title is required' })}
+          error={!!errors.title}
+          helperText={errors.title?.message}
+          sx={{ mb: 2 }}
+        />
       </form>
     </Box>
   )
