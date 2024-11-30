8b0be416fc43212e76b4ebba8630e71411e71cad
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-11-30 14:22:30 -0400

    Add description prop to RecipeCard in HomePage component

--- recipe-sharing-app/src/pages/HomePage.jsx
+++ recipe-sharing-app/src/pages/HomePage.jsx
@@ -18,7 +18,7 @@ function HomePage() {
             <Grid container spacing={3}>
                 {recipes.map((recipe) => (
                     <Grid item xs={12} sm={6} md={4} key={recipe.id}>
-                        <RecipeCard title={recipe.title} image={recipe.image} />
+                        <RecipeCard title={recipe.title} description={recipe.description} image={recipe.image} />
                     </Grid>
                 ))}
             </Grid>
