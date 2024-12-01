1f4c39d392159b91c4dbe37ebc8e23f8018b7ed7
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-12-01 17:31:16 -0400

    Add json-server setup with pagination routing for recipes

---
+++ recipe-sharing-app/server.js
@@ -0,0 +1,16 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+// Reescribir rutas para manejar paginación.
+server.use(middlewares);
+server.use(jsonServer.rewriter({
+  "/recipes?_page=:page&_limit=:limit": "/recipes"
+}));
+
+server.use(router);
+
+server.listen(5000, () => {
+  console.log('JSON Server is running on http://localhost:5000');
+});
