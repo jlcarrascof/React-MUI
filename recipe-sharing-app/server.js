b59d0988c344b93677ef40962fb2cb2d40ef4bac
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-12-02 13:53:55 -0400

    Initialize Express app and set up server port in server.js

--- recipe-sharing-app/server.js
+++ recipe-sharing-app/server.js
@@ -1,3 +1,6 @@
 const express = require('express')
 const cors = require('cors')
 const recipes = require('./db.json').recipes // Import recipes from db.json
+
+const app = express();
+const PORT = 5000;
