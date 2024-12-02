27aa5d0b90c2678c8281403cd8950e4cf258dfcc
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-12-02 13:54:43 -0400

    Add middleware for CORS and JSON parsing in server.js

--- recipe-sharing-app/server.js
+++ recipe-sharing-app/server.js
@@ -4,3 +4,7 @@ const recipes = require('./db.json').recipes // Import recipes from db.json
 
 const app = express();
 const PORT = 5000;
+
+// Middleware
+app.use(cors()); // Let access from React
+app.use(express.json());
