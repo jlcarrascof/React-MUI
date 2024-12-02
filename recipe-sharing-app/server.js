9b5d4249a03b44ff0a26943c8a93a19a312c9901
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-12-02 14:03:23 -0400

    Refactor server.js to remove unnecessary semicolons and improve code consistency

--- recipe-sharing-app/server.js
+++ recipe-sharing-app/server.js
@@ -6,15 +6,15 @@ const app = express()
 const PORT = 5000
 
 // Middleware
-app.use(cors()); // Let access from React
-app.use(express.json());
+app.use(cors()) // Let access from React
+app.use(express.json())
 
 app.get('/recipes', (req, res) => {
-    const page = parseInt(req.query._page) || 1;
-    const limit = parseInt(req.query._limit) || 2;
+    const page = parseInt(req.query._page) || 1
+    const limit = parseInt(req.query._limit) || 2
     const start = (page - 1) * limit
     const end = page * limit
 
     res.setHeader('X-Total-Count', recipes.length) // Add the header with the total of the recipes.
-    res.json(recipes.slice(start, end)); // Return the paginated recipes.
-});
+    res.json(recipes.slice(start, end)) // Return the paginated recipes.
+})
